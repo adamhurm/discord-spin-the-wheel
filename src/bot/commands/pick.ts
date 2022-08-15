e7e53ffbb6f77df4206d11de377fd63ad48d2a0b
Adam Hurm <achurm@mrrpi>
2022-08-14 20:18:44 -0400

    add Super Pal Bot restriction

--- src/bot/commands/pick.ts
+++ src/bot/commands/pick.ts
@@ -11,70 +11,76 @@ const DURATION = MAX_DURATION_MS / FRAME_DELAY_MS;
 
 const styles = {
   canvas: {
-    width: 250,
-    height: 250
+    width: 500,
+    height: 500
   }
 } as const;
 
 const pickCommand = async (args: string[], receivedMessage: Message) => {
-  const options = parseArgsAsCsv(args);
+  // Require Super Pal Bot permissions for spinning the wheel.
+  if(receivedMessage.member?.roles.cache.some(role => role.name === 'Super Pal Bot')) {
+    const options = parseArgsAsCsv(args);
 
-  if (!options || options.length < 2) {
-    await receivedMessage.channel.send('ℹ Usage: _!pick name1,name2,name3_');
-    return;
-  }
+    if (!options || options.length < 2) {
+      await receivedMessage.channel.send('ℹ Usage: _!pick name1,name2,name3_');
+      return;
+    }
 
-  const loadingMessageEmbed = new MessageEmbed()
-    .setTitle('Wheel of names')
-    .setDescription('Creating the wheel, wait a moment...');
-
-  const loadingMessage = await receivedMessage.channel.send(
-    loadingMessageEmbed
-  );
-
-  const randomEndAngle = Math.random() * (MAX_ANGLE - MIN_ANGLE) + MIN_ANGLE;
-
-  const wheel = generateSpinWheel(
-    options,
-    randomEndAngle,
-    DURATION,
-    FRAME_DELAY_MS,
-    styles.canvas.width,
-    styles.canvas.height,
-    LAST_FRAME_DURATION_MS
-  );
-
-  loadingMessage.delete();
-
-  const spinWheelAttachment = new MessageAttachment(
-    wheel.getGif(),
-    'spin-wheel.gif'
-  );
-
-  const message = await receivedMessage.channel.send(
-    new MessageEmbed()
+    const loadingMessageEmbed = new MessageEmbed()
       .setTitle('Wheel of names')
-      .setImage('attachment://spin-wheel.gif')
-      .attachFiles([spinWheelAttachment])
-  );
+      .setDescription('Creating the wheel, wait a moment...');
 
-  setTimeout(async () => {
-    await message.delete();
-
-    const selectedOptionAttachmment = new MessageAttachment(
-      wheel.getLastFrame(),
-      'last-option.png'
+    const loadingMessage = await receivedMessage.channel.send(
+      loadingMessageEmbed
     );
 
-    await receivedMessage.channel.send(
+    const randomEndAngle = Math.random() * (MAX_ANGLE - MIN_ANGLE) + MIN_ANGLE;
+
+    const wheel = generateSpinWheel(
+      options,
+      randomEndAngle,
+      DURATION,
+      FRAME_DELAY_MS,
+      styles.canvas.width,
+      styles.canvas.height,
+      LAST_FRAME_DURATION_MS
+    );
+
+    loadingMessage.delete();
+
+    const spinWheelAttachment = new MessageAttachment(
+      wheel.getGif(),
+      'spin-wheel.gif'
+    );
+
+    const message = await receivedMessage.channel.send(
       new MessageEmbed()
-        .setColor(wheel.selectedOptionColor)
         .setTitle('Wheel of names')
-        .setDescription(`🏆 Winner: **${wheel.selectedOption}**`)
-        .setImage('attachment://last-option.png')
-        .attachFiles([selectedOptionAttachmment])
+        .setImage('attachment://spin-wheel.gif')
+        .attachFiles([spinWheelAttachment])
     );
-  }, MAX_DURATION_MS);
+
+    setTimeout(async () => {
+      await message.delete();
+
+      const selectedOptionAttachmment = new MessageAttachment(
+        wheel.getLastFrame(),
+        'last-option.png'
+      );
+
+      await receivedMessage.channel.send(
+        new MessageEmbed()
+          .setColor(wheel.selectedOptionColor)
+          .setTitle('Wheel of names')
+          .setDescription(`🏆 Winner: **${wheel.selectedOption}**`)
+          .setImage('attachment://last-option.png')
+          .attachFiles([selectedOptionAttachmment])
+      );
+    }, MAX_DURATION_MS);
+  }
+  else {
+    receivedMessage.channel.send('Only the Super Pal Bot can use this command.')
+  }
 };
 
 export { pickCommand };
