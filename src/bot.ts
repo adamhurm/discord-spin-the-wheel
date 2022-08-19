4503663a2986f5c5a4915870bc32726223df160e
Adam Hurm <adamhurm@gmail.com>
2022-08-18 22:30:19 -0400

    fix relative reference

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,6 @@
 import { Client } from 'discord.js';
 import { processCommand } from './bot/commands';
-require('dotenv').config({ path: path.resolve(__dirname, '../.env')});
+require('dotenv').config({ path: '../.env'});
 
 const client = new Client();
 
